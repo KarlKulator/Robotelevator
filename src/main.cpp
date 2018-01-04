a121c136fc9ed985a0ba7882b6fc848136ab04f9
KarlKulator <7453230+KarlKulator@users.noreply.github.com>
2018-01-04 15:31:59 +0100

    setup main

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,22 @@
  *      Author: veit
  */
 
-int main(){
+#include "statemachine/RobotelevatorStatemachine.h"
+#include "ServoswitchedElevatorMotor.h"
+#include "Buttons.h"
 
+#include <chrono>
+#include <thread>
+
+int main(){
+	ServoswitchedElevatorMotor motor;
+	RobotelevatorStatemachine statemachine(&motor);
+	Buttons buttons(&statemachine);
+
+	statemachine.startCleanup();
+
+	while(true)
+	   std::this_thread::sleep_for(std::chrono::hours::max());
 }
 
 
